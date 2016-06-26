a322d9f5f3c143a62086f6e6434faadd9fbb9380
Yoshi Sakai <yoshi.sakai@gmail.com>
2016-06-26 21:56:10 +0900

    wait a id tag

--- test/example.js
+++ test/example.js
@@ -28,9 +28,9 @@ describe ('login and logout at rakuten.co.jp', function() {
                 .type('input#login_id', config.app.login.username)
                 .type('input#password', config.app.login.password)
                 .click('html body div#w div#main-my.page-login.d-rst div.wrap div.area-login div.sect div.box-account dl.box dd form.validator.login div#loginbutton_script_on.box-btn-login span.btn-login.btn input')
-                .wait(2000)
+                .wait('#l-side')
                 .screenshot('./logs/screenshots/login.png')
-                .wait(250)
+                .wait(50)
                 .evaluate(function () {
                     return location.origin + location.pathname;
                 });
@@ -42,11 +42,10 @@ describe ('login and logout at rakuten.co.jp', function() {
                 .goto('http://book.dmm.com/detail/b510ckaka00871/')
                 .wait()
                 .screenshot('./logs/screenshots/logout.png')
-                .wait(250)
+                .wait(50)
                 .evaluate(function() {
                     return document.getElementsByClassName('m-boxSubDetailPurchase__price__value')[0].innerText;
-                })
-                ;
+                });
 		location.should.eql('626円');
         });
 
