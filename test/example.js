71c0d3e29127b216706b0e8c5d6952f18a8c9513
Yoshi Sakai <yoshi.sakai@gmail.com>
2016-06-26 21:59:48 +0900

    nightmare show to top

--- test/example.js
+++ test/example.js
@@ -3,24 +3,11 @@ require('mocha-generators').install();
 var Nightmare = require('nightmare');
 var config = require('./config');
 var should = require('chai').should();
+var nightmare = Nightmare({ show: true });
 
 describe ('login and logout at rakuten.co.jp', function() {
-
-    /** 楽天にログイン、ログアウト */
-    describe ('login and logout', function () {
-
-        var nightmare
-
-        beforeEach (function() {
-            nightmare = Nightmare({ show: true });
-        });
-
-        afterEach (function*() {
-            yield nightmare.end();
-        });
-
+    describe ('login', function () {
         it ('login', function*() {
-
             var location = yield nightmare
                 .viewport(1024,768)
                 .goto(config.app.login.url)
@@ -30,25 +17,22 @@ describe ('login and logout at rakuten.co.jp', function() {
                 .click('html body div#w div#main-my.page-login.d-rst div.wrap div.area-login div.sect div.box-account dl.box dd form.validator.login div#loginbutton_script_on.box-btn-login span.btn-login.btn input')
                 .wait('#l-side')
                 .screenshot('./logs/screenshots/login.png')
-                .wait(50)
+                .wait(10)
                 .evaluate(function () {
                     return location.origin + location.pathname;
                 });
             location.should.eql('http://www.dmm.com/');
         });
-
         it ('logout', function*() {
             var location = yield nightmare
                 .goto('http://book.dmm.com/detail/b510ckaka00871/')
                 .wait()
                 .screenshot('./logs/screenshots/logout.png')
-                .wait(50)
+                .wait(10)
                 .evaluate(function() {
                     return document.getElementsByClassName('m-boxSubDetailPurchase__price__value')[0].innerText;
                 });
 		location.should.eql('626円');
         });
-
     });
-
 });
